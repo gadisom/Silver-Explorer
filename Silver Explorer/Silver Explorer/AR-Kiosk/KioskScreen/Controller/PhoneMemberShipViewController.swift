bb14adfcafbe9dcbdf5ed11689d2aa129e81daec
Jinyoung Yoo <jinyoo@Jinyoungui-MacBookPro.local>
2023-07-31 01:59:58 +0900

    Design: 상품 옵션 선택 오토레이아웃 수정 및 멤버쉽 적립 오토레이아웃 완료

---
+++ Silver Explorer/Silver Explorer/AR-Kiosk/KioskScreen/Controller/PhoneMemberShipViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PhoneMemberShipViewController.swift
+//  Silver Explorer
+//
+//  Created by Jinyoung Yoo on 2023/07/31.
+//
+
+import UIKit
+
+class PhoneMemberShipViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
