d3807c65843fd73d0016e78576e56a3436e83941
Jinyoung Yoo <jinyoo@Jinyoungui-MacBookPro.local>
2023-08-02 21:48:36 +0900

    Feat: 상품 옵션 모달 구현 완료 및 멤버쉽 선택 모달 초기 구현

--- Silver Explorer/Silver Explorer/AR-Kiosk/KioskScreen/Controller/MembershipViewController.swift
+++ Silver Explorer/Silver Explorer/AR-Kiosk/KioskScreen/Controller/MembershipViewController.swift
@@ -8,22 +8,31 @@
 import UIKit
 
 class MembershipViewController: UIViewController {
-
+    
+    @IBOutlet weak var barcodeMembershipContainerView: UIView!
+    @IBOutlet weak var phoneMembershipContainerView: UIView!
+    @IBOutlet weak var membershipSelectSegmentedControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        initialSettingForSegmentControl()
+    }
 
-        // Do any additional setup after loading the view.
+    @IBAction func membershipMethodSelected(_ sender: UISegmentedControl) {
+        if (sender.selectedSegmentIndex == 0) {
+            barcodeMembershipContainerView.isHidden = true
+            phoneMembershipContainerView.isHidden = false
+        } else {
+            phoneMembershipContainerView.isHidden = true
+            barcodeMembershipContainerView.isHidden = false
+        }
     }
     
+    func initialSettingForSegmentControl() {
+        barcodeMembershipContainerView.isHidden = true
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        let textAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        membershipSelectSegmentedControl.setTitleTextAttributes(textAttributes, for: .normal)
     }
-    */
-
 }
