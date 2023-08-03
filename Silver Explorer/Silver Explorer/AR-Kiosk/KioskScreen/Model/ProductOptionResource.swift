dabd3f8cc9b6a68abfedd100b28b15e2fd766b8d
Jinyoung Yoo <jinyoo@Jinyoungui-MacBookPro.local>
2023-08-03 21:50:51 +0900

    Feat: KioskMainBoardDelegate 정의 및 사용 코드 추가

--- Silver Explorer/Silver Explorer/AR-Kiosk/KioskScreen/Model/ProductOptionResource.swift
+++ Silver Explorer/Silver Explorer/AR-Kiosk/KioskScreen/Model/ProductOptionResource.swift
@@ -14,3 +14,26 @@ struct ProductOptionResource {
         .beverage: #imageLiteral(resourceName: "beverageOptionImg")
     ]
 }
+
+struct Product {
+    let productName: String
+    let productType: ProductType
+    let singleProductPrice: Int
+    let totalProductPrice: Int
+    
+    // KioskMainBoard에서 넘겨줄 때 사용할 이니셜라이저
+    init(productName: String, productType: ProductType, price: Int) {
+        self.productName = productName
+        self.productType = productType
+        self.singleProductPrice = price
+        self.totalProductPrice = 0
+    }
+    
+    // ProductOptionSelect에서 넘겨줄 때 사용할 이니셜라이저
+    init(productName: String, singleProductPrice: Int, totalProductPrice: Int) {
+        self.productName = productName
+        self.productType = .none
+        self.singleProductPrice = singleProductPrice
+        self.totalProductPrice = totalProductPrice
+    }
+}
