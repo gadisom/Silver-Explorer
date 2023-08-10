38b3fd7c5e062bebbb84b24ee597cf9ef5f66243
Jinyoung Yoo <jinyoo@Jinyoungui-MacBookPro.local>
2023-08-11 02:34:48 +0900

    Feat: 각 탐험 콘텐츠 설명 화면 구현

---
+++ Silver Explorer/Silver Explorer/Delegate/HomeDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  HomeDelegate.swift
+//  Silver Explorer
+//
+//  Created by Jinyoung Yoo on 2023/08/09.
+//
+
+import Foundation
+
+protocol HomeDelegate: AnyObject {
+    func content() -> Content
+}
--- Silver Explorer/Silver Explorer/Utils/Enum.swift
+++ Silver Explorer/Silver Explorer/Utils/Enum.swift
@@ -10,7 +10,7 @@ import Foundation
 // MARK: - Home Enum
 
 enum Content: String {
-    case UIExplore, ARKiosk, AIExplore
+    case UIExplore, ARKiosk, AIExplore, none
 }
 
 // MARK: - AR-Kiosk Enum
